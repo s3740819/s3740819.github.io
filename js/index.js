01aba978663a63d004c77e016ba6dbef3f362d8b
s3740819 <s3740819@rmit.edu.vn>
2021-10-09 01:31:51 +0700

    Update index.js

--- js/index.js
+++ js/index.js
@@ -27,7 +27,8 @@ function showSlides(n) {
             var content = document.getElementById("content");
             content.innerHTML = "<b style='color: burlywood;'>Github: </b> <a href='https://github.com/zd247/cocangua'>Click Here!</a></br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> Java, Javafx, CSS, game development</br></br>";
-            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Jul, 2019 - Sep, 2019";
+            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Jul, 2019 - Sep, 2019</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
         else if (n == 2){
             document.getElementById("img").setAttribute("src", "img/embed2.png");
@@ -35,7 +36,8 @@ function showSlides(n) {
             var content = document.getElementById("content");
             content.innerHTML = "<b style='color: burlywood;'>Github: </b> <a href='https://github.com/s3740819/Embedded-Design'>Click Here!</a></br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> Embedded C, game development</br></br>";
-            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Jul, 2019 - Aug, 2019";
+            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Jul, 2019 - Aug, 2019</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
         else if (n == 3){
             document.getElementById("img").setAttribute("src", "img/qa.png");
@@ -43,7 +45,8 @@ function showSlides(n) {
             var content = document.getElementById("content");
             content.innerHTML = "<b style='color: burlywood;'>Github: </b> <a href='https://github.com/s3740819/QA-Testing'>Click Here!</a></br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> Java, Spring framework, QA Testing skills</br></br>";
-            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Mar, 2020 - Apr, 2020";
+            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Mar, 2020 - Apr, 2020</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
         else if (n == 4){
             document.getElementById("img").setAttribute("src", "img/home.png");
@@ -51,7 +54,8 @@ function showSlides(n) {
             var content = document.getElementById("content");
             content.innerHTML = "<b style='color: burlywood;'>Github: </b> <a href='https://github.com/hoangdesu/myRMIT'>Click Here!</a></br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> Android development with Java, Firebase, FireStore, Google SDKs and APIs</br></br>";
-            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Jul, 2020 - Aug, 2020";
+            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Jul, 2020 - Aug, 2020</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
         else if (n == 5){
             document.getElementById("img").setAttribute("src", "img/PiCar.jpg");
@@ -59,7 +63,8 @@ function showSlides(n) {
             var content = document.getElementById("content");
             content.innerHTML = "<b style='color: burlywood;'>Github: </b> <a href='https://github.com/hoang-10n/EEET2610_Design_3'>Click Here!</a></br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> IoT development, Python programming, Embedded System development, Computer Vision</br></br>";
-            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Mar, 2020 - Apr, 2020";
+            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Mar, 2020 - Apr, 2020</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
         else if (n == 6){
             document.getElementById("img").setAttribute("src", "img/drug.png");
@@ -67,7 +72,8 @@ function showSlides(n) {
             var content = document.getElementById("content");
             content.innerHTML = "<b style='color: burlywood;'>Github: </b> <a href='https://github.com/javatalent115/Enterprise-Assignment'>Click Here!</a></br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> HTML, CSS, JavaScript, Spring framework, PostgreSQL</br></br>";
-            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Aug, 2021 - Sep, 2021";
+            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Aug, 2021 - Sep, 2021</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
         else if (n == 7){
             document.getElementById("img").setAttribute("src", "img/covid.png");
@@ -75,7 +81,8 @@ function showSlides(n) {
             var content = document.getElementById("content");
             content.innerHTML = "<b style='color: burlywood;'>Github: </b> <a href='https://github.com/javatalent115/cloud-assigment'>Click Here!</a></br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> Cloud computing, AWS, nodeJS, reactJS</br></br>";
-            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Aug, 2021 - Sep, 2021";
+            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Aug, 2021 - Sep, 2021</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
         else if (n == 8){
             document.getElementById("img").setAttribute("src", "img/game.png");
@@ -83,7 +90,9 @@ function showSlides(n) {
             var content = document.getElementById("content");
             content.innerHTML = "<b style='color: burlywood;'>Github: </b> <a href='https://github.com/s3740819/Embedded-System-OS-and-Interfacing'>Click Here!</a></br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> Embedded C, Image Processing, Bare Metal OS development</br></br>";
-            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Aug, 2021 - Sep, 2021";
+            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Aug, 2021 - Sep, 2021</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
+
         }
     }
 }
