f8f1c3e65d9234410b3fe4f08ad675987b2b13b6
s3740819 <s3740819@rmit.edu.vn>
2021-10-23 01:53:21 +0700

    update

--- js/index.js
+++ js/index.js
@@ -16,7 +16,17 @@ function showSlides(n) {
         }
         dots[n-1].className += " active";
 
-        if(n == 1){
+        if (n == 1){
+            document.getElementById("img").setAttribute("src", "img/fish.png");
+            document.getElementById("title").innerHTML = "<h2><b>Mini Fish game with Web development</b></h2> </br>";
+            var content = document.getElementById("content");
+            content.innerHTML = "<b style='color: burlywood;'>Github: </b> <a href='https://github.com/s3740819/Fish-game'>Click Here!</a></br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> Web development (html,css,js), Game development</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Date:</b> Feb, 2019 (2 days)</br></br>";
+            content.innerHTML += "<b style='color: burlywood;'>Company:</b> Self Study Project";
+
+        }
+        else if(n == 2){
             document.getElementById("img").setAttribute("src", "img/cocangua.png");
             document.getElementById("title").innerHTML = "<h2><b>Parcheesi game</b></h2> </br>";
             var content = document.getElementById("content");
@@ -25,7 +35,7 @@ function showSlides(n) {
             content.innerHTML += "<b style='color: burlywood;'>Date:</b> Jul, 2019 - Sep, 2019</br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
-        else if (n == 2){
+        else if (n == 3){
             document.getElementById("img").setAttribute("src", "img/embed2.png");
             document.getElementById("title").innerHTML = "<h2><b>Embedded System Applications with Nuvoton board</b></h2> </br>";
             var content = document.getElementById("content");
@@ -34,7 +44,7 @@ function showSlides(n) {
             content.innerHTML += "<b style='color: burlywood;'>Date:</b> Jul, 2019 - Aug, 2019</br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
-        else if (n == 3){
+        else if (n == 4){
             document.getElementById("img").setAttribute("src", "img/qa.png");
             document.getElementById("title").innerHTML = "<h2><b>QA Testing project</b></h2> </br>";
             var content = document.getElementById("content");
@@ -43,7 +53,7 @@ function showSlides(n) {
             content.innerHTML += "<b style='color: burlywood;'>Date:</b> Mar, 2020 - Apr, 2020</br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
-        else if (n == 4){
+        else if (n == 5){
             document.getElementById("img").setAttribute("src", "img/home.png");
             document.getElementById("title").innerHTML = "<h2><b>My RMIT application</b></h2> </br>";
             var content = document.getElementById("content");
@@ -52,7 +62,7 @@ function showSlides(n) {
             content.innerHTML += "<b style='color: burlywood;'>Date:</b> Jul, 2020 - Aug, 2020</br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
-        else if (n == 5){
+        else if (n == 6){
             document.getElementById("img").setAttribute("src", "img/PiCar.jpg");
             document.getElementById("title").innerHTML = "<h2><b>Autonomous car with Raspberry Pi</b></h2> </br>";
             var content = document.getElementById("content");
@@ -61,7 +71,7 @@ function showSlides(n) {
             content.innerHTML += "<b style='color: burlywood;'>Date:</b> Mar, 2020 - Apr, 2020</br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
-        else if (n == 6){
+        else if (n == 7){
             document.getElementById("img").setAttribute("src", "img/drug.png");
             document.getElementById("title").innerHTML = "<h2><b>Drug Store Online (Web)</b></h2> </br>";
             var content = document.getElementById("content");
@@ -70,7 +80,7 @@ function showSlides(n) {
             content.innerHTML += "<b style='color: burlywood;'>Date:</b> Aug, 2021 - Sep, 2021</br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
-        else if (n == 7){
+        else if (n == 8){
             document.getElementById("img").setAttribute("src", "img/covid.png");
             document.getElementById("title").innerHTML = "<h2><b>Covid19 Behavior Management (Web)</b></h2> </br>";
             var content = document.getElementById("content");
@@ -79,7 +89,7 @@ function showSlides(n) {
             content.innerHTML += "<b style='color: burlywood;'>Date:</b> Aug, 2021 - Sep, 2021</br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
         }
-        else if (n == 8){
+        else if (n == 9){
             document.getElementById("img").setAttribute("src", "img/game.png");
             document.getElementById("title").innerHTML = "<h2><b>Bare Metal OS development (Crossy Road Game) with Raspberry Pi</b></h2> </br>";
             var content = document.getElementById("content");
@@ -87,7 +97,6 @@ function showSlides(n) {
             content.innerHTML += "<b style='color: burlywood;'>Skill Set Utilized: </b> Embedded C, Image Processing, Bare Metal OS development</br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Date:</b> Aug, 2021 - Sep, 2021</br></br>";
             content.innerHTML += "<b style='color: burlywood;'>Company:</b> RMIT University Vietnam (School Project)";
-
         }
 
         setTimeout(()=>{
